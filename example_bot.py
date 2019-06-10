7af172d069a5a1234d02481cb328e9504b871152
migoB1 <46myago@gmail.com>
2019-06-10 22:36:48 +0900

    Update example_bot.py

--- example_bot.py
+++ example_bot.py
@@ -11,7 +11,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('よお'):
+    if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
 client.run('your token here')
